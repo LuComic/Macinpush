df85bc3333168f690fd92db4a24fa1252e2b0d88
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-07-28 21:30:03 +0300

    files

---
+++ macinpush.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.run(['osascript', '-e', 'display notification "Hello from PythonHello from PythonHello from PythonHello from PythonHello from PythonHello from PythonHello from PythonHello from PythonHello from PythonHello from Python" with title "Utility"'])
